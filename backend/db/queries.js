ff6ace80ba50ace7343453783bdb5b05abf5e510
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2025-03-10 02:45:02 +0500

    add comments db queries

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -46,7 +46,7 @@ const db = {
   // get all posts
   getallPosts: async () => {
     const posts = await prisma.post.findMany({});
-    console.log(posts);
+    // console.log(posts);
     return posts;
   },
   getAllPostsByWriterId: async (writerId) => {
@@ -58,7 +58,7 @@ const db = {
   // create post
   createPost: async (title, body, writerId) => {
     try {
-      console.log({ title, body, writerId });
+      // console.log({ title, body, writerId });
       await prisma.post.create({
         data: { title, body, writerId },
       });
@@ -82,11 +82,31 @@ const db = {
   // check if it is published or not
   // get writer by post id
   // ---------------------------
-  // get all comments for a post
+  // get all comments
+  getComments: async () => {
+    return await prisma.comments.findMany({});
+  },
+  getCommentsForPostId: async (postId) => {
+    const comments = await prisma.comments.findMany({ where: { postId } });
+    // console.log(comments);
+    return comments;
+  },
   // create comment
+  createComment: async (username, email, postId, content) => {
+    // if (typeof postId !== "number") {
+    //   postId = +postId;
+    //   console.log(typeof postId);
+    // }
+    await prisma.comments.create({
+      data: { username, email, content, postId },
+    });
+  },
   // delete comment make sure that only writer can do that
-  //
+  deleteComment: async (postId, commentId) => {
+    await prisma.comments.delete({ where: { id: commentId, postId } });
+  },
 };
+// db.createComment("whatever", "w@w", 34, "just a test comment");
 // db.deletePost(23)
 // db.editPost(33, "hehe(edited)", "whatever", 1);
 // db.createWriter("jill1", "jill1@jill.com", "3123213", true);
@@ -97,4 +117,6 @@ const db = {
 // db.getAllCatagories();
 // db.createPost("title of post 1", "body of post 1",1);
 // db.getallPosts();
+// db.getCommentsForPostId(34);
+// db.getComments();
 module.exports = db;
