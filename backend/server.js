c745b67be82cc5d12d257951e5cf92f7c5730851
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2025-03-10 02:45:18 +0500

    add route /comments

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,16 @@ const express = require("express");
 const rootRouter = require("./routes/rootRouter");
 const postsRouter = require("./routes/postsRouter");
 const catagoriesRouter = require("./routes/catagoriesRouter");
+const commentsRouter = require("./routes/commentsRouter");
 const app = express();
 require("dotenv").config();
-app.use(express.json())
+app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 app.use("/", rootRouter);
 app.use("/api/v1/posts", postsRouter);
 app.use("/api/v1/catagory", catagoriesRouter);
+app.use("/api/v1/comments", commentsRouter);
 
 const port = process.env.PORT;
 app.listen(port, () => {
